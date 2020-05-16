a39c75c7bb4a7e2bff2559c78ad8e38838e8bd84
Ubuntu <ubuntu@ip-172-31-33-185.us-west-2.compute.internal>
2020-05-16 11:50:22 +0000

    final model

--- model.py
+++ model.py
@@ -1,41 +1,85 @@
+## Import the necessary libraries
+
+import os
 import csv
 import cv2
+import sklearn
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Lambda, Conv2D, MaxPool2D, Cropping2D, Flatten, Dense,Dropout 
+from keras.callbacks import ModelCheckpoint 
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 
-lines = []
+## Read the csv file for extraction of path of the image
 
-with open('image_data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
+lines = []  
+with open('./new_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader, None)
+    for line in reader :
+        lines.append(line)
 
-images = []
+## Spliting dataset to trainset and validation set
+training_data, validation_data = train_test_split(lines,test_size=0.2)
 
-measurements = []
+### Create generator 
+def generator(lines, batch_size=64):
+    len_data = len(lines)
+    while 1: 
+        shuffle(lines)
+        for offset in range(0, len_data, batch_size):
+            batch_data = lines[offset:offset+batch_size]
+            images = []
+            angles = []
+            for line in batch_data:
+                path = './new_data/IMG/'+line[0].split('/')[-1]
+                center_image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB) 
+                center_angle = float(line[3])
+                images.append(center_image)
+                angles.append(center_angle)            
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            
+            yield sklearn.utils.shuffle(X_train, y_train)
+            
+## Creating training and validation generator
 
-for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = '/Users/bat/image_data/IMG/' + filename
-	image =  cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+train_generator = generator(training_data, batch_size=64)
+validation_generator = generator(validation_data, batch_size=64)
 
 
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Lambda
-
+## Define architecture
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+
+model.add(Conv2D(24, (5, 5), strides =(2,2), activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+          
+model.add(Conv2D(36, (5, 5), strides =(2,2), activation="relu"))
+model.add(Dropout(0.3))
+          
+model.add(Conv2D(48, (5, 5), strides =(2,2), activation="relu"))
+model.add(Dropout(0.3))
+
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50)) 
+model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer = 'adam')
-model.fit(X_train,y_train, validation_split = 0.2, shuffle = True,epochs=5)
-model.save('model.h5')
+## Checkpointer for saving the best model
+checkpointer = ModelCheckpoint(filepath='model.h5', 
+                               verbose=1, save_best_only=True)
+
+## Compile the model with mse loss and adam optimizer          
+model.compile(loss='mse', optimizer='adam')
+
+## Training the model
+model.fit_generator(train_generator, validation_steps=len(validation_data), epochs=10, 
+                    validation_data=validation_generator, steps_per_epoch= len(training_data),callbacks=[checkpointer])
