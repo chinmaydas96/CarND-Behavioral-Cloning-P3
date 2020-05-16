db878f807d8a7d478f31e174b161450c302f768b
Ubuntu <ubuntu@ip-172-31-33-185.us-west-2.compute.internal>
2020-05-16 12:44:13 +0000

    update model

--- model.py
+++ model.py
@@ -7,10 +7,12 @@ import sklearn
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Lambda, Conv2D, MaxPool2D, Cropping2D, Flatten, Dense,Dropout 
-from keras.callbacks import ModelCheckpoint 
+from keras.callbacks import ModelCheckpoint
+from keras.utils import plot_model
 
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+import random
 
 
 ## Read the csv file for extraction of path of the image
@@ -25,24 +27,60 @@ with open('./new_data/driving_log.csv') as csvfile:
 ## Spliting dataset to trainset and validation set
 training_data, validation_data = train_test_split(lines,test_size=0.2)
 
+
+def make_dataset(line, images, measurements):
+    """
+    This function reads data from line and adds it to the list of input images
+    and measurements
+    """
+
+    # correction for left and right camera image
+    steering_angle_correction = 0.2
+
+    # load images and measurements
+    
+    "./new_data/" + line[0].replace('/Users/bat/image_data/','')
+    image_center = cv2.imread("./new_data/" + line[0].replace('/Users/bat/image_data/',''))
+    image_left = cv2.imread("./new_data/" + line[0].replace('/Users/bat/image_data/',''))
+    image_right = cv2.imread("./new_data/" + line[0].replace('/Users/bat/image_data/',''))
+    steering_angle = float(line[3])
+    # correct angles for left and right image
+    steering_angle_left = steering_angle + steering_angle_correction
+    steering_angle_right = steering_angle - steering_angle_correction
+
+    # add original and flipped images to the list of images
+    images.append(image_center)
+    images.append(cv2.flip(image_center,1))
+    images.append(image_left)
+    images.append(cv2.flip(image_left,1))
+    images.append(image_right)
+    images.append(cv2.flip(image_right,1))
+
+    # add corresponting measurements
+    measurements.append(steering_angle)
+    measurements.append(-steering_angle)
+    measurements.append(steering_angle_left)
+    measurements.append(-steering_angle_left)
+    measurements.append(steering_angle_right)
+    measurements.append(-steering_angle_right)
+
+
+
 ### Create generator 
-def generator(lines, batch_size=64):
-    len_data = len(lines)
-    while 1: 
-        shuffle(lines)
-        for offset in range(0, len_data, batch_size):
-            batch_data = lines[offset:offset+batch_size]
+def generator(samples, batch_size=64):
+    num_samples = len(samples)
+    while 1:
+        random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset : offset + batch_size]
+
             images = []
             angles = []
-            for line in batch_data:
-                path = './new_data/IMG/'+line[0].split('/')[-1]
-                center_image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB) 
-                center_angle = float(line[3])
-                images.append(center_image)
-                angles.append(center_angle)            
+            for batch_sample in batch_samples:
+                make_dataset(batch_sample, images, angles)
+
             X_train = np.array(images)
             y_train = np.array(angles)
-            
             yield sklearn.utils.shuffle(X_train, y_train)
             
 ## Creating training and validation generator
@@ -54,8 +92,8 @@ validation_generator = generator(validation_data, batch_size=64)
 ## Define architecture
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
 model.add(Conv2D(24, (5, 5), strides =(2,2), activation="relu"))
 model.add(MaxPool2D(pool_size=(2, 2)))
@@ -72,6 +110,9 @@ model.add(Dense(100))
 model.add(Dense(50)) 
 model.add(Dense(10))
 model.add(Dense(1))
+
+model_image = './images/architecture.png'
+plot_model(model, to_file=model_image, show_shapes=True)
 
 ## Checkpointer for saving the best model
 checkpointer = ModelCheckpoint(filepath='model.h5', 
